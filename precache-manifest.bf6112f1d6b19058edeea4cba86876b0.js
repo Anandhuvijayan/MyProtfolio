f33790d09eb92d3310771f712cdc80cbdf513117
Anandhu Vijayan <anandhuvijayan400@gmail.com>
2020-07-31 21:12:36 +0200

    Updates

---
+++ precache-manifest.bf6112f1d6b19058edeea4cba86876b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022a487f82af0346ccb5f4519a29c7d0",
+    "url": "/MyProtfolio/index.html"
+  },
+  {
+    "revision": "1c463a2f990a26d6baf8",
+    "url": "/MyProtfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f813b5ed9d879e68bb3e",
+    "url": "/MyProtfolio/static/js/2.7ba041ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyProtfolio/static/js/2.7ba041ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c463a2f990a26d6baf8",
+    "url": "/MyProtfolio/static/js/main.3875dfe3.chunk.js"
+  },
+  {
+    "revision": "b2478ab3549cb1a9dc35",
+    "url": "/MyProtfolio/static/js/runtime-main.418b3d90.js"
+  }
+]);
